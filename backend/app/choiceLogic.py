c07d0b328ce239ff2b10edd62033b1804abd6f6c
Sucy <hybaek807@naver.com>
2025-07-23 17:36:32 +0900

    Add choiceLogic.py

---
+++ backend/app/choiceLogic.py
@@ -0,0 +1,12 @@
+from veganLens import extract_text
+# 이곳에 각자 만든 ocr 추출 함수 임포트 하기
+
+def choice(image, debug=True, base_filename=None, who='BHY'):
+    if (who == 'BHY'):
+        return extract_text(image, debug=True, base_filename=base_filename)
+    elif (who == 'HSK'):
+        return '' # 이곳에 서경님의 ocr 추출 함수 적기
+    elif (who == 'IMY'):
+        return '' # 이곳에 문영님의 ocr 추출 함수 적기
+    else:
+        return print('다시 입력')
