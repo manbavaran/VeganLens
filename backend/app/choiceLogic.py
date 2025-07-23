3323895a0b85d7d5d0678d0d4744110cbd72baa1
Sucy <hybaek807@naver.com>
2025-07-23 19:35:08 +0900

    modify choice function

--- backend/app/choiceLogic.py
+++ backend/app/choiceLogic.py
@@ -2,13 +2,13 @@ from .veganLens import extract_text
 # 이곳에 각자 만든 ocr 추출 함수 임포트 하기
 
 def choice(image, debug=True, base_filename=None, version = 1, who='BHY'):
-    if (who == 'BHY' & version == 1):
+    if (who == 'BHY' and version == 1):
         return extract_text(image, debug=True, base_filename=base_filename, version=1)
-    elif (who == 'BHY' & version == 2):
+    elif (who == 'BHY' and version == 2):
         return extract_text(image, debug=True, base_filename=base_filename, version=2)
-    elif (who == 'HSK' & version == 1):
+    elif (who == 'HSK' and version == 1):
         return '' # 이곳에 서경님의 ocr 추출 함수 적기
-    elif (who == 'IMY' & version == 1):
+    elif (who == 'IMY' and version == 1):
         return '' # 이곳에 문영님의 ocr 추출 함수 적기
     else:
         return print('다시 입력')
